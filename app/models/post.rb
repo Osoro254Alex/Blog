ad8174a31fa9d280ad4f29f3582cffb7388d1c2d
Alex Ogeto <alexogeto54@gmail.com>
2023-03-29 20:42:10 +0300

    Updated posts.rb files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.where(posts_id: id).order(created_at: :desc).limit(5)
+  end
 end
