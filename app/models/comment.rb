ea597efff510657f0063b940b711b8262f175f8a
Alex Ogeto <alexogeto54@gmail.com>
2023-03-30 16:48:28 +0300

    Test setup

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,9 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_counter
   belongs_to :author, class_name: 'User'
   belongs_to :post
-  after_save :update_comments_counter
-
-  private
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.all.length)
   end
 end
