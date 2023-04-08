316471514a600e43cb9f298f16950f3d70b06142
Alex Ogeto <alexogeto54@gmail.com>
2023-04-08 18:24:14 +0300

    Modify app/controllers/posts_controller.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,21 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(author_id: params[:post][:user_id], title: params[:post][:title], text: params[:post][:text].gsub(/\r\n?/, '<br>'))
+
+    if @post.save
+      redirect_to user_path(params[:post][:user_id]), notice: 'Post was successfully created.'
+    else
+      flash.alert = 'Error!, Post not added!'
+      render :new
+    end
   end
 end
