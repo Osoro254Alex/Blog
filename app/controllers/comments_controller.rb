41e7adad2e5fa1cce4f0978fc8254fcd02f07895
Alex Ogeto <alexogeto54@gmail.com>
2023-04-08 19:10:38 +0300

    Modify app/controllers/comments_controller.rb file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
   def create
     @comment = Comment.create(author_id: current_user.id, post_id: params[:post_id], text: params[:comment][:text])
     if @comment.save
