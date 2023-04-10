235c3b6ee90bf8fb162dee483963c6afc823765c
Alex Ogeto <alexogeto54@gmail.com>
2023-04-10 20:10:01 +0300

    Fix Linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,8 @@ class PostsController < ApplicationController
   def show
     user_id = params[:user_id]
     post_id = params[:id]
-    @user = User.find(user_id)
     @post = Post.find(post_id)
+    @user = User.find(user_id)
     @comment = Comment.new
   end
 
@@ -22,8 +22,8 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to user_path(params[:post][:user_id]), notice: 'Post was successfully created.'
     else
-      flash.alert = 'Error!, Post not added!'
       render :new
+      flash.alert = 'Error!, Post not added!'
     end
   end
 end
