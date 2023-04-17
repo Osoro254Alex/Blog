fe645a2edac8921333a0a2f653dcb7282038d461
Alex Ogeto <alexogeto54@gmail.com>
2023-04-17 13:40:50 +0300

    Add styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.all.includes(:posts)
+    @users = User.all.order(:id).includes(:posts)
   end
 
   def show
