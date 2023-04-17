8a92800b3e019659d39839c4ca38fa41af69fb58
Alex Ogeto <alexogeto54@gmail.com>
2023-04-17 21:29:54 +0300

    Comment APIs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,31 @@
 class CommentsController < ApplicationController
+
+    def index
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comments = @post.comments
+    respond_to do |format|
+      format.html { redirect_to user_post_path(params[:user_id], params[:post_id]) }
+      format.json { render json: @comments.as_json(only: %i[id text author_id post_id]) }
+    end
+  end
+
   def create
     @comment = Comment.create(author_id: current_user.id, post_id: params[:post_id], text: params[:comment][:text])
-    if @comment.save
-      redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Comment was successfully created.'
-    else
-      render :new
+      respond_to do |format|
+      if @comment.save
+        format.html do
+          redirect_to user_post_path(@comment.author, @comment.post), notice: 'Comment was successfully created.'
+        end
+        format.json do
+          render json: { status: 'success', message: 'Comment created successfully', data: @comment }, status: :created
+        end
+      else
+        format.html { render :new }
+        format.json do
+          render json: { status: 'error', message: 'Failed to create comment' }, status: :unprocessable_entity
+        end
+      end
     end
   end
 
